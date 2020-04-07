a13ad1b81b3c548fa5b41a0022e39a7d3ce0dc80
Tung Tran <tst008@bucknell.edu>
2020-04-08 06:52:37 +0700

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Spring 2020
+ * Instructor: Prof. Chris Dancy
+ *
+ * Name: Tung Tran
+ * Section: 8am
+ * Date: 4/8/2020
+ * Time: 6:42 AM
+ *
+ * Project: csci205FinalProjectSP2020
+ * Package: main
+ * Class: Main
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
